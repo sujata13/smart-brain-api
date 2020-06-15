d34fed8589c6c58cf118ab58a302f36abcfa4b22
Sujata Mishra <mishrasujata1628@gmail.com>
2020-06-15 20:59:29 +0530

    remove retun in register

--- controllers/register.js
+++ controllers/register.js
@@ -12,7 +12,7 @@ const handleRegister = (req,res,db,bcrypt) => {
         .into('login')
         .returning('email')
         .then(loginEmail =>{
-            return trx('users')
+             trx('users')
                 .returning('*')
                 .insert({
                 email:loginEmail[0],
