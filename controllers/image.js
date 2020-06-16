b2d05105ed7f3511c8f49ad0748cde614c9b1fdf
Sujata Mishra <mishrasujata1628@gmail.com>
2020-06-16 12:46:43 +0530

    Change to err resp of Api

--- controllers/image.js
+++ controllers/image.js
@@ -9,7 +9,7 @@ const handleApiCall = (req,res) => {
        .then(data =>{
            res.json(data);
        })
-       .catch(err => res.status(400).json('unable to work with API'))
+       .catch(err => res.status(400).json(err))
 }
 
 const handleImage = (req,res, db) =>{
